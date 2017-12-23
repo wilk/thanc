943b8482b05118dc8571dd2a3257e4b1c23d4362
Wilk <wilk3ert@gmail.com>
2017-12-23 04:01:19 +0100

    Add first draft of thanc

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:9.3.0-alpine
+
+WORKDIR /opt/app
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,131 @@
+const RegistryClient = require('npm-registry-client')
+const client = new RegistryClient()
+const NPM_REGISTRY_URL = 'https://registry.npmjs.org'
+const GITHUB_API_URL = 'https://api.github.com'
+const GITHUB_STAR_URL = `${GITHUB_API_URL}/user/starred`
+const axios = require('axios')
+const prompt = require('prompt')
+const fs = require('fs')
+const util = require('util')
+const asyncReadFile = util.promisify(fs.readFile)
+const EXIT_FAILURE = 1
+const DEFAULT_PACKAGE_LOCK_PATH = `${__dirname}/package-lock.json`
+
+const schema = {
+  properties: {
+    username: {
+      description: 'Your github username',
+      type: 'string',
+      required: true
+    },
+    password: {
+      description: 'Your github password',
+      type: 'string',
+      hidden: true,
+      required: true
+    },
+    packageLockPath: {
+      description: 'Your package-lock.json path',
+      type: 'string',
+      default: DEFAULT_PACKAGE_LOCK_PATH
+    }
+  }
+}
+
+// get user info
+const getUserInfo = schema => {
+  return new Promise((resolve, reject) => {
+    prompt.get(schema, function (err, data) {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+(async () => {
+  prompt.start()
+
+  // getting credentials from the user
+  let userInfo
+  try {
+    console.log('Requesting user info...')
+    userInfo = await getUserInfo(schema)
+    console.log('done!')
+  } catch (err) {
+    console.log('Cannot fetch github user credentials')
+    console.error(err)
+
+    process.exit(EXIT_FAILURE)
+  }
+
+  // testing credentials
+  const auth = {username: userInfo.username, password: userInfo.password}
+  try {
+    console.log('Testing github credentials...')
+    await axios({url: GITHUB_API_URL, method: 'get', auth})
+    console.log('done!')
+  } catch (err) {
+    console.log('Invalid credentials or maximum number of login attempts exceeded')
+    console.error(err)
+
+    process.exit(EXIT_FAILURE)
+  }
+
+  // reading package-lock.json
+  let pkg
+  try {
+    console.log('Reading package-lock.json...')
+    const rawPackageLock = await asyncReadFile(userInfo.packageLockPath, 'utf-8')
+    pkg = JSON.parse(rawPackageLock)
+    console.log('done!')
+  } catch (err) {
+    console.log('Cannot read package-lock.json')
+    console.error(err)
+
+    process.exit(EXIT_FAILURE)
+  }
+
+  // generating deps repos promises
+  const depsPromises = Object.keys(pkg.dependencies).map(dep => {
+    return new Promise((resolve, reject) => {
+      client.get(`${NPM_REGISTRY_URL}/${dep}`, {}, (err, data) => {
+        if (err) reject(err)
+        else resolve(data.versions[pkg.dependencies[dep].version])
+      })
+    })
+  })
+
+  // getting deps repos
+  let deps = []
+  try {
+    console.log('Getting dependencies...')
+    deps = await Promise.all(depsPromises)
+    console.log('done!')
+  } catch (err) {
+    console.log('Cannot fetch dependencies\' repos')
+    console.error(err)
+
+    process.exit(EXIT_FAILURE)
+  }
+
+  // generating repos object: keys are repos and values are owners
+  const repos = {}
+  deps.forEach((detail) => {
+    if (!detail.repository) return
+
+    const splitUrl = detail.repository.url.split('/')
+    repos[splitUrl[splitUrl.length - 1].replace('.git', '')] = splitUrl[splitUrl.length - 2]
+  })
+
+  // starring repos
+  try {
+    console.log('Starring dependencies...')
+    await Promise.all(Object.keys(repos).map(repo => axios({url: `${GITHUB_STAR_URL}/${repos[repo]}/${repo}`, method: 'put', auth})))
+    console.log('done!')
+  } catch (err) {
+    console.log('Cannot star dependencies')
+    console.error(err)
+
+    process.exit(EXIT_FAILURE)
+  }
+})()
